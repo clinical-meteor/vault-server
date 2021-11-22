d1d4b942b925e109a319f78ba8baff77487c9b39
Abigail Watson <awatson1978@gmail.com>
2021-11-22 17:53:25 -0600

    fork and publish; v7.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'clinical:vault-server-freemium',
-    version: '6.4.4',
+    version: '7.0.0',
     summary: 'Add FHIR API endpoints to your Node on FHIR application.',
     git: 'https://github.com/clinical-meteor/fhir-vault-server'
 });
