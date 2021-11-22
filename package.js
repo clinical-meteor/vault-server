24c39357f7a96cd36f0076addd0afbc58c66fc24
Abigail Watson <awatson@mitre.org>
2021-11-22 17:59:18 -0600

    bump dep

--- package.js
+++ package.js
@@ -28,7 +28,7 @@ Package.onUse(function(api) {
     // FHIR data layer
     api.use('simple:json-routes@2.1.0');
     api.use('clinical:hl7-resource-datatypes@4.0.5');
-    api.use('clinical:hl7-fhir-data-infrastructure');
+    api.use('clinical:hl7-fhir-data-infrastructure@6.17.0');
 
     // REST Endpoints
     api.addFiles('FhirServer/Core.js', 'server');
