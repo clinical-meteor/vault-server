305965bb70e7cb9d97cff8aea93d126b35804d18
Abigail Watson <awatson@mitre.org>
2022-01-11 15:17:58 -0600

    udap registration response

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'clinical:vault-server',
-    version: '8.0.10',
+    version: '8.0.11',
     summary: 'Add FHIR API endpoints to your Node on FHIR application.',
     git: 'https://github.com/clinical-meteor/vault-server'
 });
