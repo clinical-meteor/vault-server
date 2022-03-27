8a534d87f358cdae667e2260f1bf0dd488cc30fb
Abigail Watson <awatson@mitre.org>
2022-03-27 13:33:08 -0500

    Touchstone, Inferno, UDAP bugfixes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'clinical:vault-server',
-    version: '8.3.42',
+    version: '8.3.43',
     summary: 'Add FHIR API endpoints to your Node on FHIR application.',
     git: 'https://github.com/clinical-meteor/vault-server'
 });
