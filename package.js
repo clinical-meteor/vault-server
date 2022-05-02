d7fa8f4e5a21988e7050510ff06c61716acb2603
Abigail Watson <awatson@mitre.org>
2022-05-01 22:45:27 -0500

    Subscriptions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'clinical:vault-server',
-    version: '8.3.62',
+    version: '8.3.63',
     summary: 'Add FHIR API endpoints to your Node on FHIR application.',
     git: 'https://github.com/clinical-meteor/vault-server'
 });
