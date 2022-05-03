79e8cf47817b8ddbb38d2ca7398e2ef1bed99f90
Abigail Watson <awatson@mitre.org>
2022-05-03 16:05:44 -0500

    bump deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'clinical:vault-server',
-    version: '8.3.66',
+    version: '8.3.67',
     summary: 'Add FHIR API endpoints to your Node on FHIR application.',
     git: 'https://github.com/clinical-meteor/vault-server'
 });
@@ -30,7 +30,7 @@ Package.onUse(function(api) {
     // FHIR data layer
     api.use('clinical:json-routes@2.3.0');
     api.use('clinical:hl7-resource-datatypes@4.0.5');
-    api.use('clinical:hl7-fhir-data-infrastructure@6.23.6');
+    api.use('clinical:hl7-fhir-data-infrastructure@6.23.7');
 
     // REST Endpoints
     api.addFiles('FhirServer/main.js', 'server');
