356bd767adfa106a8507a9d7182f093fc2e8cc26
Abigail Watson <awatson@mitre.org>
2022-07-20 04:20:12 -0500

    backend services tunnel; basic auth (hardcoded)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'clinical:vault-server',
-    version: '8.4.26',
+    version: '8.4.27',
     summary: 'Add FHIR API endpoints to your Node on FHIR application.',
     git: 'https://github.com/clinical-meteor/vault-server'
 });
