7e1a7a52e88a5e257ac35e780193324cac519e15
Abigail Watson <awatson@mitre.org>
2022-07-19 23:56:02 -0500

    protected womens shelters

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'clinical:vault-server',
-    version: '8.4.24',
+    version: '8.4.25',
     summary: 'Add FHIR API endpoints to your Node on FHIR application.',
     git: 'https://github.com/clinical-meteor/vault-server'
 });
